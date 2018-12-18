fa4757f4f70cf00d626ecfc1503cd72e9b1f0097
zhangcx <zhangcx@zhangcxdeiMac.lan>
2018-12-18 21:29:16 +0800

    使用HMR不刷新页面的前提下 主动更新页面元素或者对应的绑定时间

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import printMe from './print'
+import printMe from './print.js'
 function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button')
@@ -13,11 +13,14 @@ function component() {
     return element;
   }
   
-  document.body.appendChild(component());
+  let element = component()
+  document.body.appendChild(element);
 
   if(module.hot) {
     module.hot.accept('./print.js', function() {
       console.log('Accepting the updated printMe module!')
-      printMe
+      document.body.removeChild(element)
+      element = component()
+      document.body.appendChild(element)
     })
   }
