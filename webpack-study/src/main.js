a1507b3fa3ddd8b63006bb444ec098ad83c5cc42
zhangcx <zhangcx@zhangcxdeiMac.lan>
2019-08-22 17:57:02 +0800

    start demo

--- webpack-study/src/main.js
+++ webpack-study/src/main.js
@@ -1,8 +1,10 @@
 import $ from 'jquery'
+import './css/index.css'
+import './css/index.less'
 
 $(function(){
     $('li:odd').css('background-color', 'blue')
     $('li:even').css('background-color', function() {
-        return 'red';
+        return 'yellow';
     })
 })
--- webpack-study/webpack.config.js
+++ webpack-study/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const webpack = require('webpack')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: path.join(__dirname, './src/main.js'),
     output: {
@@ -10,5 +13,22 @@ module.exports = {
         open: true,
         hot: true,
         contentBase: 'src'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new htmlWebpackPlugin({
+            template: path.join(__dirname, './src/index.html'),
+            filename: 'index.html'
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.css$/, 
+            use: ['style-loader','css-loader']
+        }, {
+            test: /\.less$/,
+            use: ['style-loader', 'css-loader', 'less-loader']
+        }
+        ]
     }
 }
